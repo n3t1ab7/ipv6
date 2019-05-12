9d0434e013cec847987a64f47276b96eae667442
黄博 <n3t1ab7@outlook.com>
2019-05-13 02:29:14 +0800

    Update ip.py

--- ip.py
+++ ip.py
@@ -11,18 +11,20 @@ import os,sys
 import random
 from IPy import IP
 
-IPV6ADDR="2604:180:2:1334::/64"
+IPV6ADDR = input("IPV6ADDR:")
+#IPV6ADDR="2604:180:2:1334::/64"
 
-ipv6_addr_fn="ipv6_addr.txt"
-
-if(os.path.exists(ipv6_addr_fn)):
-    os.remove(ipv6_addr_fn)
+if(os.path.exists("10000.txt")):
+    os.remove("10000.txt")
 
 ipv6s = IP(IPV6ADDR)
 len = ipv6s.len()
 
 # random.randint(0,99)
 #   print(ipv6s.len())
+
+ipv6_addr_fn="ipv6_addr.txt"
+
 
 fp = open(ipv6_addr_fn, "a+")
 
