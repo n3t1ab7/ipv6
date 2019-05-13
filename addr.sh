464957b60a9e6aeb225c3555cd04ccd111341a09
黄博 <n3t1ab7@outlook.com>
2019-05-13 08:50:00 +0800

    Update addr.sh

--- addr.sh
+++ addr.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-ipv6_addr_fn="ipv6_addr.txt"
 
-for ipaddr in `cat $ipv6_addr_fn`
-do
-    addr=`ip a | grep inet6 | grep $ipaddr`
-    if [ -z "$addr" ]
-    then
-        echo $ipaddr
-        ifconfig eth0 inet6 add $ipaddr
-        usleep 100      #毫秒级别的sleep
-    fi
-done
+add_ipv6_fun(){
 
+    ipv6_addr_fn="ipv6_addr.txt"
+    for ipaddr in `cat $ipv6_addr_fn`
+    do
+        addr=`ip a | grep inet6 | grep $ipaddr`
+        if [ -z "$addr" ]
+        then
+            echo $ipaddr
+            ifconfig eth0 inet6 add $ipaddr
+            usleep 100      #毫秒级别的sleep
+        fi
+    done
+}
 
+ip -6 address show | grep inet6 | awk '{print $2}' | grep 2604  > locate-ipv6.txt
 
+addr_num=`cat ipv6_addr.txt | wc -l`
 
+sys_addr_ipv6=`cat locate-ipv6.txt | wc -l`
 
-
-
-
-
-
-
-
-
-
+if [ $sys_addr_ipv6 -gt $addr_num ]; 
+then   
+    echo "add IPV6 sucss"
+else
+    add_ipv6_fun
+fi
 
 
 
